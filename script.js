30fe120f55615456d621195333d7073a797d4859
Nicolas Rodriguez <94021624+NicRodri@users.noreply.github.com>
2022-05-03 13:35:00 -0700

    Add functionality to arithmetic operations

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+let currentNum = null;
+let previousNum = null;
+let operation = null;
+displayValues = "";
 function add(a, b){
-    return a+b;
+    return Number(a) + Number(b);
 }
 function subtract(a, b){
-    return a-b;
+    return Number(a) - Number(b);
 }
 function multiply(a, b){
-    return a*b;
+    return Number(a) * Number(b);
 }
 function divide(a, b){
     if(b == 0){
         return "Error Division by 0";
     }
     else{
-        return a/b;
+        return Number(a) / Number(b);
     }
 }
 
@@ -30,5 +34,50 @@ function operate(operator, a, b){
         return divide(a, b);
     }  
 }
+function displayValue(){ // Displays and stores the values of numbers selected
+    const display = document.getElementById("display")
+    display.innerHTML = displayValues;
+}
 
+function storeNumbers(){
+    const numButtons = document.querySelectorAll(".num")
+    numButtons.forEach((numButton) => {
+        numButton.addEventListener("click", () =>{
+
+            displayValues += numButton.id;
+            previousNum = currentNum;
+            currentNum = numButton.id;
+            displayValue();
+            console.log("current    " + currentNum);
+            console.log("previous   " + previousNum);
+        });
+    });
+}
+function calculator(){
+    storeNumbers();
+    const equals = document.getElementById("equals");
+    const clear = document.getElementById("clear");
+    const operators = document.querySelectorAll(".operation");
+
+    operators.forEach((operator) =>{
+        operator.addEventListener("click", () =>{
+            displayValues += operator.innerHTML;
+            displayValue();
+            operation = operator.id;
+            console.log(operation);
+        });
+    });
+    clear.addEventListener("click", () => {
+        displayValues = "";
+        displayValue();
+    });
+    equals.addEventListener("click", () => {
+        
+        displayValues = operate(operation, previousNum, currentNum);
+        displayValue();
+    });
+    
+}
+
+calculator();
 //console.log(operate("multiply", 20, 10));
