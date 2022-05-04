93fc036fbdf139c2ff2809d5016de4236271ef55
Nicolas Rodriguez <94021624+NicRodri@users.noreply.github.com>
2022-05-03 17:04:27 -0700

    add keyboard implementation

--- script.js
+++ script.js
@@ -1,109 +1,256 @@
-let currentNum = null;
-let previousNum = null;
-let operation = null;
-let operationDisplayed = null;
-let decimalUse = false;
-displayValues = "";
 
-function add(a, b){
-    let num = Number(a) + Number(b);
-    return +num.toFixed(10);// Used toFixed used to round numbers. Rounding is inaccurate with values ending in 5 over 10 digits.
-}
-function subtract(a, b){
-    let num = Number(a) - Number(b);
-    return +num.toFixed(10);
-}
-function multiply(a, b){
-    let num = Number(a) * Number(b);
-    return +num.toFixed(10);
-}
-function divide(a, b){
+function calculator(){ //Runs the calculator
+    let currentNum = null;
+    let previousNum = null;
+    let operation = null;
+    let operationDisplayed = null;
+    let decimalUse = false;
+    displayValues = "";
     
-    if(b == 0){
-        return "Error Division by 0";
+    function add(a, b){
+        let num = Number(a) + Number(b);
+        return +num.toFixed(10);// Used toFixed used to round numbers. Rounding is inaccurate with values ending in 5 over 10 digits.
     }
-    else{
-        let num = Number(a) / Number(b);
+    function subtract(a, b){
+        let num = Number(a) - Number(b);
         return +num.toFixed(10);
     }
-}
-
-function operate(operator, a, b){    
-    if(operator == "add"){
-        return add(a, b);
+    function multiply(a, b){
+        let num = Number(a) * Number(b);
+        return +num.toFixed(10);
     }
-    else if(operator == "subtract"){
-        return subtract(a, b);
+    function divide(a, b){
+        
+        if(b == 0){
+            return "Error Division by 0";
+        }
+        else{
+            let num = Number(a) / Number(b);
+            return +num.toFixed(10);
+        }
     }
-    else if(operator == "multiply"){
-        return multiply(a, b);
+    
+    function operate(operator, a, b){    
+        if(operator == "add"|| operator == "+"){
+            return add(a, b);
+        }
+        else if(operator == "subtract" || operator == "-"){
+            return subtract(a, b);
+        }
+        else if(operator == "multiply" || operator ==  "*"){
+            return multiply(a, b);
+        }
+        else if(operator == "divide" || operator == "/"){
+            return divide(a, b);
+        }  
     }
-    else if(operator == "divide"){
-        return divide(a, b);
-    }  
-}
-function displayValue(){ //Displays the screen with numbers and current operations
-    const display = document.getElementById("display")
-    display.innerHTML = displayValues;
-}
+    function displayValue(){ //Displays the screen with numbers and current operations
+        const display = document.getElementById("display")
+        display.innerHTML = displayValues;
+    }
+    
+    function storeNum(){ //Stores the currentNumber used for calculations
+        const numButtons = document.querySelectorAll(".num")
+        numButtons.forEach((numButton) => {
+            numButton.addEventListener("click", () =>{
+    
+                displayValues += numButton.id;
+                if (currentNum == null){
+                    currentNum = numButton.id;
+                }
+                else{
+                    currentNum += numButton.id;
+                }
+    
+                displayValue();
+                console.log("current    " + currentNum);
+                console.log("previous   " + previousNum);
+            });
+        });
+    }
 
-function storeNum(){ //Stores the currentNumber used for calculations
-    const numButtons = document.querySelectorAll(".num")
-    numButtons.forEach((numButton) => {
-        numButton.addEventListener("click", () =>{
-
-            displayValues += numButton.id;
-            if (currentNum == null){
-                currentNum = numButton.id;
+    function backspace(){ //Adds backspace functionality to calculator
+        const backspace = document.getElementById("backspace");
+        backspace.addEventListener("click", () =>{ 
+            if(displayValues.charAt(displayValues.length -1) != operationDisplayed){
+                if(displayValues.charAt(displayValues.length -1) == "."){
+                    decimalUse= false;
+                }
+                displayValues = displayValues.slice(0, -1);
+                displayValue();
+                currentNum = currentNum.slice(0, -1);
             }
-            else{
-                currentNum += numButton.id;
-            }
+        });
+    }
 
+    function decimal(){ //Adds the decimal functionality to calculator
+        const decimal = document.getElementById(".");
+        decimal.addEventListener("click", () =>{ 
+            if(decimalUse== false && currentNum!= null){
+                displayValues+=decimal.innerHTML;
+                currentNum+= decimal.innerHTML;
+                displayValue();
+                decimalUse= true;
+                console.log(decimalUse);
+    
+            }
+        });
+    }
+
+    function equals(){ //Operates on the current pair of numbers based on last arithmetic operator used
+        const equals = document.getElementById("equals");
+        equals.addEventListener("click", () => { 
+            console.log("current    " + currentNum);
+            console.log("previous   " + previousNum);
+            if(currentNum!= null && currentNum!="" && previousNum!= null && displayValues.charAt(displayValues.length -1) != "."){
+    
+                displayValues = String(operate(operation, previousNum, currentNum));
+                displayValue();
+                currentNum = displayValues;
+                previousNum = null;
+    
+                if(previousNum =="Error Division by 0" || currentNum =="Error Division by 0"){
+                    alert("Division by 0!");
+                    displayValues = "";
+                    currentNum = null;
+                    previousNum = null;
+                    decimalUse = false;
+                    displayValue();
+                }
+    
+                console.log("current    " + currentNum);
+            }
+        });
+    }
+
+    function clear(){ //Removes all stored and displayed numbers
+        const clear = document.getElementById("clear");
+        clear.addEventListener("click", () => { 
+            displayValues = "";
+            currentNum = null;
+            previousNum = null;
+            decimalUse = false;
             displayValue();
             console.log("current    " + currentNum);
             console.log("previous   " + previousNum);
         });
-    });
-}
-function calculator(){ //Runs the calculator
+    }
+
+    function operators(){ //Applies arithmetic operators and updates stored numbers
+        const operators = document.querySelectorAll(".operation");
+        operators.forEach((operator) =>{ 
+            operator.addEventListener("click", () =>{
+                if(currentNum!=null&&currentNum!=""&& displayValues.charAt(displayValues.length -1) != "."){
+                    displayValues += operator.innerHTML;
+                    displayValue();
+                    
+                    if(previousNum == null){
+                        previousNum = currentNum;
+                        console.log("previous   " + previousNum); 
+                    }
+        
+                    else{
+                        previousNum = operate(operation, previousNum, currentNum);
+                        console.log("previous   " + previousNum);
+                    }
+                    currentNum = null;
+        
+                    if(previousNum == "Error Division by 0"){
+                        alert("Error Division by 0!");
+                        displayValues = "";
+                        currentNum = null;
+                        previousNum = null;
+                        decimalUse = false;
+                        displayValue();
+                    }
+                    operation = operator.id;
+                    operationDisplayed = operator.innerHTML;
+                    console.log(operation);
+                    decimalUse = false;
+                }
+    
+            });
+        });
+    }
     storeNum(); //Used to store and display numbers used
+    backspace();
+    decimal();
+    equals();
+    clear();
+    operators();
 
-    const equals = document.getElementById("equals");
-    const clear = document.getElementById("clear");
-    const operators = document.querySelectorAll(".operation");
-    const decimal = document.getElementById(".");
-    const backspace = document.getElementById("backspace");
+    
 
-    backspace.addEventListener("click", () =>{ //Adds backspace functionality to calculator
-        if(displayValues.charAt(displayValues.length -1) != operationDisplayed){
-            if(displayValues.charAt(displayValues.length -1) == "."){
-                decimalUse= false;
+    window.addEventListener("keydown", (e) =>{
+        if(e.key == 1|| e.key == 2 || e.key == 3 || e.key == 4 || e.key == 5 || e.key == 6 || e.key == 7 || e.key == 8 || e.key == 9 || e.key == 0){ //Numbers input
+            displayValues += e.key;
+            if (currentNum == null){
+                currentNum = e.key;
             }
-            displayValues = displayValues.slice(0, -1);
+            else{
+                currentNum += e.key;
+            }
+
             displayValue();
-            currentNum = currentNum.slice(0, -1);
             console.log("current    " + currentNum);
             console.log("previous   " + previousNum);
-            console.log(displayValues.charAt(displayValues.length -1));
-            console.log(operation);
         }
-    });
+        else if(e.key == "Backspace"){ //Backspace input
+            if(displayValues.charAt(displayValues.length -1) != operationDisplayed){
+                if(displayValues.charAt(displayValues.length -1) == "."){
+                    decimalUse= false;
+                }
+                displayValues = displayValues.slice(0, -1);
+                displayValue();
+                currentNum = currentNum.slice(0, -1);
+            }
 
-    decimal.addEventListener("click", () =>{ //Adds the decimal functionality 
-        if(!decimalUse&& currentNum!= null){
-            displayValues+=decimal.innerHTML;
-            currentNum+= decimal.innerHTML;
+        }
+        else if(e.key == "."){ //Decimal input
+            if(decimalUse == false  && currentNum!= null){
+                displayValues+=e.key;
+                currentNum+= e.key;
+                displayValue();
+                decimalUse= true;
+                console.log(decimalUse);
+            }
+        }
+        else if(e.key == "Delete"){ //Clear input(uses delete button)
+            displayValues = "";
+            currentNum = null;
+            previousNum = null;
+            decimalUse = false;
             displayValue();
-            decimalUse= true;
-            console.log(decimalUse);
-
+            console.log("current    " + currentNum);
+            console.log("previous   " + previousNum);
+         
         }
-    });
-    operators.forEach((operator) =>{ //Takes actions onto numbers if an operator is clicked
-        operator.addEventListener("click", () =>{
+        else if(e.key == "="|| e.key == "Enter"){ //Equals input
+
+            console.log("current    " + currentNum);
+            console.log("previous   " + previousNum);
+            if(currentNum!= null && currentNum!="" && previousNum!= null && displayValues.charAt(displayValues.length -1) != "."){
+    
+                displayValues = String(operate(operation, previousNum, currentNum));
+                displayValue();
+                currentNum = displayValues;
+                previousNum = null;
+    
+                if(previousNum =="Error Division by 0" || currentNum =="Error Division by 0"){
+                    alert("Division by 0!");
+                    displayValues = "";
+                    currentNum = null;
+                    previousNum = null;
+                    decimalUse = false;
+                    displayValue();
+                }
+    
+                console.log("current    " + currentNum);
+            }
+        }
+        else if(e.key == "+" || e.key == "-" || e.key == "*" || e.key == "/"){ //Operation input
             if(currentNum!=null&&currentNum!=""&& displayValues.charAt(displayValues.length -1) != "."){
-                displayValues += operator.innerHTML;
+                displayValues += e.key;
                 displayValue();
                 
                 if(previousNum == null){
@@ -125,46 +272,19 @@ function calculator(){ //Runs the calculator
                     decimalUse = false;
                     displayValue();
                 }
-                operation = operator.id;
-                operationDisplayed = operator.innerHTML;
+                operation = e.key;
+                operationDisplayed = e.key;
                 console.log(operation);
                 decimalUse = false;
             }
-
-        });
-    });
-    clear.addEventListener("click", () => { //Removes all stored and displayed numbers
-        displayValues = "";
-        currentNum = null;
-        previousNum = null;
-        decimalUse = false;
-        displayValue();
-        console.log("current    " + currentNum);
-        console.log("previous   " + previousNum);
-    });
-    equals.addEventListener("click", () => { //Operates on the current pair of numbers
-        console.log("current    " + currentNum);
-        console.log("previous   " + previousNum);
-        if(currentNum!= null && currentNum!="" && previousNum!= null && displayValues.charAt(displayValues.length -1) != "."){
-
-            displayValues = String(operate(operation, previousNum, currentNum));
-            displayValue();
-            currentNum = displayValues;
-            previousNum = null;
-
-            if(previousNum =="Error Division by 0" || currentNum =="Error Division by 0"){
-                alert("Division by 0!");
-                displayValues = "";
-                currentNum = null;
-                previousNum = null;
-                decimalUse = false;
-                displayValue();
-            }
-
-            console.log("current    " + currentNum);
+            console.log("operation");
         }
     });
-    
+
+
+
+
+
 }
 
 calculator();
